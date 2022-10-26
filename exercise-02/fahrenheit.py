b98cc158addb13d41c69b5b20c3be431198a0831
Paul Maier <pauljustus279@gmail.com>
2022-10-26 14:01:35 +0200

    Only execute code if script is run directly

--- exercise-02/fahrenheit.py
+++ exercise-02/fahrenheit.py
@@ -2,5 +2,6 @@ def celsiusToFahrenheit(celsius: float) -> float:
     return celsius * 9.0 / 5.0 + 32
 
 
-celsius = float(input('Celsius: '))
-print('Fahrenheit: %.2f' % celsiusToFahrenheit(celsius))
+if __name__ == '__main__':
+    celsius = float(input('Celsius: '))
+    print('Fahrenheit: %.2f' % celsiusToFahrenheit(celsius))
